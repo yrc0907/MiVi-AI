62246912b6bf128aaef070d3076815da2595f550
yrc0907 <3616104980@qq.com>
2025-06-15 14:09:22 +0800

    新增github登录

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
-export default NextAuth(authConfig).auth;
+export default auth((req) => {
+  if (
+    !req.auth &&
+    req.nextUrl.pathname !== "/login" &&
+    req.nextUrl.pathname !== "/register"
+  ) {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
