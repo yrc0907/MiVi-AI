563632b9a514943b4da83a191f12aee0c40640e7
yrc0907 <3616104980@qq.com>
2025-06-27 14:01:17 +0800

    新增模糊搜索，分页bug修复

--- drizzle/0000_lyrical_frog_thor.sql
+++ drizzle/0000_lyrical_frog_thor.sql
@@ -91,7 +91,7 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "session" ADD CONSTRAINT "session_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
